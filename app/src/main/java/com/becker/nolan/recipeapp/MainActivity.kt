9cc26444989b6a0e62fe3381a6eb1881e6790eb3
Nolan Becker <nolan.becker@ymail.com>
2021-04-13 13:46:46 -0500

    Add API call to get recipe list and populate the RecyclerView

--- app/src/main/java/com/becker/nolan/recipeapp/MainActivity.kt
+++ app/src/main/java/com/becker/nolan/recipeapp/MainActivity.kt
@@ -2,10 +2,64 @@ package com.becker.nolan.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONArrayRequestListener
+import com.becker.nolan.recipeapp.databinding.ActivityMainBinding
+import com.google.gson.Gson
+import org.json.JSONArray
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    val recipeList = ArrayList<Recipe>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.recipeListView.adapter = RecipeAdapter(recipeList)
+        binding.recipeListView.layoutManager = LinearLayoutManager(this)
+        binding.recipeListView.setHasFixedSize(true)
+
+        getRecipes()
+
     }
+
+    private fun getRecipes() {
+        recipeList.clear()
+
+        AndroidNetworking.initialize(this)
+        AndroidNetworking.get("https://api.nolanbecker.net/api/recipe/recipes/")
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsJSONArray(object: JSONArrayRequestListener {
+                    override fun onResponse(response: JSONArray?) {
+                        val gson = Gson()
+                        if (response != null) {
+                            for (i in 0 until response.length()) {
+                                val recipeObject = response.getJSONObject(i).toString()
+                                val recipe: Recipe = gson.fromJson(recipeObject, Recipe::class.java)
+                                recipeList.add(recipe)
+                            }
+                            binding.recipeListView.adapter?.notifyDataSetChanged()
+                        }
+                    }
+
+                    override fun onError(anError: ANError?) {
+                        if (anError != null) {
+                            anError.message?.let {
+                                Log.e("getRecipes", it)
+                            }
+                        }
+                    }
+                })
+    }
+
 }
